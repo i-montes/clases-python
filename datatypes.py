53eed23be13af7789c5a60fb8f5fefcd1287bea9
iandres.montes <ivan.montes.developer@gmail.com>
2023-08-19 20:33:19 -0300

    init

---
+++ datatypes.py
@@ -0,0 +1,97 @@
+# este es un comentario de una sola linea
+
+'''
+    Este es un
+      comentario
+'''
+
+"""
+    Multiline strings
+     can be written
+    using triple quotes
+"""
+
+#################################################################
+# 0 Variables
+#################################################################
+ty = 123
+var1 = 1
+wqwe= 123
+_pablo1_ = 123
+
+##################################################################
+# 1 Datatypes y Operators
+##################################################################
+
+#numbers (int => integer)
+number1 = 123
+
+# Maths
+var1 = 1 - 1 # resta 
+var2 = 2+1 #suma
+var3 = 2 *10 #mltiplicacion
+var4 = 10 / 2 #division
+var5 = -0
+
+# Numeros flotantes (float)
+var1 = 123.123 - 123
+
+#> Strings (str => string) Cadenas de texto
+
+var1 = "pablo es un jugador de ""minecraft"". "
+var2 = 'Este es un \"string\"'
+var3 = """  
+Hola
+    qwe\n\tpablo es un "gamer"
+"""
+
+# Concatenacion
+
+var1 = "La casa" + "roberto" + "qweqwe"
+var2 = "roberto"
+var3 = "yendo a"
+var4 = var3+ var2+ var1
+
+# concatenacion con format
+var1 = "Minecraft"
+var2 = "pablo juega {}".format(var1)
+
+# otra forma
+var1 = "minecraft"
+nombre = "pablo"
+accion = "juega"
+var2 = "%s %s %s"%(nombre, accion,var1)
+
+
+#> Booleanos (bool)
+
+var1 = True
+var2 = False
+var3 = 0 #False
+var4 = 1 #True
+var5 = [] # False
+var6 = [1,2,"123"] #True
+var7 = () # False
+var8 = (1,2,3) # True
+
+#> Arrays (list[index])
+var1 = [1,2,3,4,5,6,7,8,9,0]
+var2 = ["apple", "pineapple", "banana", "blueberry", [1,2,[1,2,3]], (1,2,3, [1,23, (123,2,3)])]
+
+# se pueden concatenar listas
+var3 = var1+var2
+
+# se puede traer un item de la lista en especifico
+var2[2]
+
+
+# Tupla (tuple)
+var1 = (1,2,3,4)
+
+
+
+
+
+
+
+
